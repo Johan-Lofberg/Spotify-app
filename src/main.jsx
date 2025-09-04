3a766cebbfb4fece5f85bf0f694dddbc68437d33
Johan-Lofberg <johan.lofberg11@gmail.com>
2025-09-04 23:42:12 +0200

    feat: add config and env setup, update App and main structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { ThemeProvider } from '@emotion/react'
+import { themeOptions } from './theme/matrial.theme'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   <ThemeProvider theme={themeOptions}>
     <App />
+   </ThemeProvider>
   </React.StrictMode>,
 )
